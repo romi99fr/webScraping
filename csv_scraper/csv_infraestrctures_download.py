5488625953d7cf5f0738c323c620a310d7262681
vvbruns <victorvalldosera@gmail.com>
2023-08-13 19:00:40 +0200

    archius .py per descargar csv datos censales

---
+++ csv_scraper/csv_infraestrctures_download.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+# URL of the website containing the CSV file
+url = "https://opendata-ajuntament.barcelona.cat/data/dataset/14f13e0d-3feb-4688-ad63-e1ca9d1e479f/resource/93d8128d-417d-44da-87cb-df7420c40641/download"
+
+
+# Specify the local directory and file name to save the downloaded CSV
+local_directory = "C:\\TFM\\webScraping\\csv_data"
+local_file_name = "Infraestructures_Inventari_Reserves.csv"
+local_file_path = os.path.join(local_directory, local_file_name)
+
+# Create the local directory if it doesn't exist
+os.makedirs(local_directory, exist_ok=True)
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Write the content of the response to the local CSV file
+    with open(local_file_path, "wb") as file:
+        file.write(response.content)
+    
+    print("CSV file downloaded successfully.")
+else:
+    print(f"Failed to download CSV file. Status code: {response.status_code}")
+
---
+++ csv_scraper/csv_taula_map_scensal_download.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+# URL of the website containing the CSV file
+url = "https://opendata-ajuntament.barcelona.cat/data/dataset/784cefda-9219-4b61-b5d5-68b5ac453070/resource/d43b851c-65a3-4e9f-9d71-33e91583b32f/download"
+
+
+# Specify the local directory and file name to save the downloaded CSV
+local_directory = "C:\\TFM\\webScraping\\csv_data"
+local_file_name = "Taula_mapa_scensal.csv"
+local_file_path = os.path.join(local_directory, local_file_name)
+
+# Create the local directory if it doesn't exist
+os.makedirs(local_directory, exist_ok=True)
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Write the content of the response to the local CSV file
+    with open(local_file_path, "wb") as file:
+        file.write(response.content)
+    
+    print("CSV file downloaded successfully.")
+else:
+    print(f"Failed to download CSV file. Status code: {response.status_code}")
+
