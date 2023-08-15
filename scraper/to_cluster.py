3eda595a245af436f456a43b86f14713c5bb5c16
Joel Romia <64154013+romi99fr@users.noreply.github.com>
2023-08-15 17:10:30 +0200

    Updated with command to save file into hdfs

--- scraper/to_cluster.py
+++ scraper/to_cluster.py
@@ -1,5 +1,10 @@
 import subprocess
 
+
+mkdir_command = "../../hadoop-2.7.4/bin/hdfs dfs -mkdir -p webScraping"
+subprocess.run(mkdir_command, shell=True)
+
 # Transfer data to HDFS using subprocess
-transfer_command = f"../../hadoop-2.7.4/bin/hdfs dfs -put ../data/data3.json webScraping"
+transfer_command = f"../../hadoop-2.7.4/bin/hdfs dfs -put ../data/data3.json webScraping/data.json"
 subprocess.run(transfer_command, shell=True)
+
