8be0ff699655604316139658f6fcf4facd5a59ab
Joel Romia <64154013+romi99fr@users.noreply.github.com>
2023-08-31 13:29:25 +0200

    Add files via upload

--- join_data/json_join_csv.py
+++ join_data/json_join_csv.py
@@ -1,20 +1,35 @@
 import pandas as pd
-
+import json
 # Leer el archivo CSV en un DataFrame
 csv_file = "../csv_data/combined_df.csv"
 csv_df = pd.read_csv(csv_file)
 
-# Leer el archivo JSON en un DataFrame
-json_file = "../data/data.json"
-json_df = pd.read_json(json_file)
 
-# Unir los DataFrames por filas (concatenación vertical)
-combined_df = pd.concat([csv_df, json_df], ignore_index=True)
+# Cargar el JSON y crear un DataFrame con las columnas necesarias
+with open('../data/data.json') as json_file:
+    json_data = json.load(json_file)
+    json_data_cleaned = []
+    for item in json_data:
+        address = item['Address']
+        distrito_parts = address['Distrito'].split(', ')
+        json_data_cleaned.append({
+            'Codi_Districte': distrito_parts[1],  # Número del distrito desde JSON
+            'Nom_Districte': distrito_parts[0],    # Nombre del distrito desde JSON
+            'Nom_Carrer': address['Calle']
+        })
+    json_df = pd.DataFrame(json_data_cleaned)
 
-# Exportar el DataFrame resultante a un nuevo archivo CSV
-combined_csv_file = "../data/final_data.csv"
-combined_df.to_csv(combined_csv_file, index=False)
+csv_df['Codi_Districte'] = csv_df['Codi_Districte'].astype(str)
+json_df['Codi_Districte'] = json_df['Codi_Districte'].astype(str)
 
-# O exportar el DataFrame resultante a un nuevo archivo JSON
-combined_json_file = "../data/final_data.json"
-combined_df.to_json(combined_json_file, orient="records", lines=True)
+# Unir los DataFrames por Codi_Districte y Nom_Districte
+merged_df = pd.merge(csv_df, json_df, left_on=['Codi_Districte', 'Nom_Districte'], right_on=['Codi_Districte', 'Nom_Districte'], how='inner')
+merged_df = merged_df.drop(columns=['NOM_CARRER'])
+column_order = ['Codi_Districte', 'Nom_Districte', 'Nom_Carrer', 'Total_Numero_Places', 'Personas', 'Total_Import_Euros', 'Vehicles']
+merged_df = merged_df[column_order]
+print(merged_df)
+
+# Guardar el resultado en un nuevo archivo CSV
+merged_df.to_csv('../data/resultado_join.csv', index=False)
+merged_df.to_json('../data/resultado_join.json', orient='records', lines=True)
+
