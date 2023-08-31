c4c157147cd55adb1b1a92d793f98761c0418f3e
Joel Romia <64154013+romi99fr@users.noreply.github.com>
2023-08-31 16:08:38 +0200

    Add files via upload

--- join_data/json_join_csv.py
+++ join_data/json_join_csv.py
@@ -5,28 +5,38 @@ csv_file = "../csv_data/combined_df.csv"
 csv_df = pd.read_csv(csv_file)
 
 
-# Cargar el JSON y crear un DataFrame con las columnas necesarias
-with open('../data/data.json') as json_file:
+with open("../data/data.json") as json_file:
     json_data = json.load(json_file)
-    json_data_cleaned = []
-    for item in json_data:
-        address = item['Address']
-        distrito_parts = address['Distrito'].split(', ')
-        json_data_cleaned.append({
-            'Codi_Districte': distrito_parts[1],  # Número del distrito desde JSON
-            'Nom_Districte': distrito_parts[0],    # Nombre del distrito desde JSON
-            'Nom_Carrer': address['Calle']
-        })
-    json_df = pd.DataFrame(json_data_cleaned)
 
-csv_df['Codi_Districte'] = csv_df['Codi_Districte'].astype(str)
-json_df['Codi_Districte'] = json_df['Codi_Districte'].astype(str)
+# Crear una lista de diccionarios con los datos del JSON
+json_data_cleaned = []
+for item in json_data:
+    address = item['Address']
+    distrito_parts = address['Distrito'].split(', ')
+    json_data_cleaned.append({
+        'Codi_Districte': distrito_parts[1],  # Número del distrito desde JSON
+        'Nom_Districte': distrito_parts[0],    # Nombre del distrito desde JSON
+        'Nom_Carrer': address['Calle'],
+        'title': item['title'],
+        'size': item['size'],
+        'price': item['price'],
+        'Vigilancia': item['Vigilancia'],
+        'Columnas': item['Columnas'],
+        'Tipo plaza': item['Tipo plaza'],
+        'Puerta': item['Puerta'],
+        'Planta': item['Planta'],
+        'Ciudad': address['Ciudad'],
+        'Total_Numero_Places': csv_df['Total_Numero_Places'],
+        'Personas': csv_df['Personas'],
+        'Total_Import_Euros': csv_df['Total_Import_Euros'],
+        'Vehicles': csv_df['Vehicles']
+    })
+
+# Crear un DataFrame a partir de la lista de diccionarios
+json_df = pd.DataFrame(json_data_cleaned)
 
 # Unir los DataFrames por Codi_Districte y Nom_Districte
 merged_df = pd.merge(csv_df, json_df, left_on=['Codi_Districte', 'Nom_Districte'], right_on=['Codi_Districte', 'Nom_Districte'], how='inner')
-merged_df = merged_df.drop(columns=['NOM_CARRER'])
-column_order = ['Codi_Districte', 'Nom_Districte', 'Nom_Carrer', 'Total_Numero_Places', 'Personas', 'Total_Import_Euros', 'Vehicles']
-merged_df = merged_df[column_order]
 print(merged_df)
 
 # Guardar el resultado en un nuevo archivo CSV
